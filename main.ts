0d537cdfd0b0f55204f48c0e01ba814040c44ccb
lucajeweetwel <91436318+lucajeweetwel@users.noreply.github.com>
2022-04-07 10:34:54 +0200

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,46 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (geel_knipperen == true) {
+        if (receivedNumber == 5) {
+            Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Green, Kitronik_STOPbit.DisplayLights.On)
+            Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Green, Kitronik_STOPbit.DisplayLights.Off)
+            Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Yellow, Kitronik_STOPbit.DisplayLights.On)
+            basic.pause(1000)
+            Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Yellow, Kitronik_STOPbit.DisplayLights.Off)
+            basic.pause(200)
+            Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Red, Kitronik_STOPbit.DisplayLights.On)
+            basic.pause(10000)
+            Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Red, Kitronik_STOPbit.DisplayLights.Off)
+            radio.sendNumber(7)
+        } else if (receivedNumber == 4) {
+            radio.sendNumber(3)
+            Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Green, Kitronik_STOPbit.DisplayLights.On)
+            basic.pause(11200)
+            Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Green, Kitronik_STOPbit.DisplayLights.Off)
+            radio.sendNumber(6)
+        }
+    } else if (geel_knipperen == false) {
+        radio.sendNumber(9)
+        Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Red, Kitronik_STOPbit.DisplayLights.Off)
+        Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Yellow, Kitronik_STOPbit.DisplayLights.Off)
+        Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Green, Kitronik_STOPbit.DisplayLights.Off)
+        for (let index = 0; index < 4; index++) {
+            Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Yellow, Kitronik_STOPbit.DisplayLights.On)
+            basic.pause(1000)
+            Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Yellow, Kitronik_STOPbit.DisplayLights.Off)
+            basic.pause(200)
+        }
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(6)
+    geel_knipperen = true
+})
+input.onButtonPressed(Button.AB, function () {
+    geel_knipperen = false
+})
+let geel_knipperen = false
+Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Green, Kitronik_STOPbit.DisplayLights.On)
+geel_knipperen = true
+basic.forever(function () {
+    radio.setGroup(169)
+})
